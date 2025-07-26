055e05171ce4a7d7452bf20c7946281378d4dd82
PavelSatyrenko <zevsp09@gmail.com>
2025-07-26 14:55:20 +0300

    feat(meetings): add create and invalid code components with associated templates and styles

---
+++ src/app/features/meetings/meetings-create/meetings-create.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-meetings-create",
+    imports: [],
+    templateUrl: "./meetings-create.html",
+    styleUrl: "./meetings-create.css"
+})
+export class MeetingsCreate {
+    
+}
---
+++ src/app/features/meetings/meetings-invalid-code/meetings-invalid-code.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-meetings-invalid-code",
+    imports: [],
+    templateUrl: "./meetings-invalid-code.html",
+    styleUrl: "./meetings-invalid-code.css"
+})
+export class MeetingsInvalidCode {
+
+}
