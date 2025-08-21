3fd5e22ef3d710d1eda6a76ce7a076147c3af4e9
Pavlo Satyrenko <zevsp09@gmail.com>
2025-08-21 14:06:20 +0300

    feat(button): enhance button component with improved event handling and new theme style

--- src/app/shared/components/button/button.ts
+++ src/app/shared/components/button/button.ts
@@ -8,12 +8,18 @@ import { Component, input, InputSignal, output, OutputEmitterRef } from "@angula
     styleUrl: "./button.css"
 })
 export class Button {
-    public readonly label: InputSignal<string | undefined> = input<string>();
-    public readonly icon: InputSignal<string | undefined> = input<string>();
-    public readonly size: InputSignal<"small" | "large" | undefined> = input<"small" | "large">();
-    public readonly type: InputSignal<"primary" | "secondary" | "primary-line" | undefined> = input<"primary" | "secondary" | "primary-line">();
-    public readonly isDisabled: InputSignal<boolean | undefined> = input<boolean>();
-    public readonly isFullWidth: InputSignal<boolean | undefined> = input<boolean>();
+    public readonly label: InputSignal<string> = input<string>("");
+    public readonly icon: InputSignal<string> = input<string>("");
+    public readonly size: InputSignal<"small" | "large"> = input<"small" | "large">("small");
+    public readonly type: InputSignal<"primary" | "secondary" | "primary-line"> = input<"primary" | "secondary" | "primary-line">("primary");
+    public readonly isDisabled: InputSignal<boolean> = input<boolean>(false);
+    public readonly isFullWidth: InputSignal<boolean> = input<boolean>(false);
 
     public readonly click: OutputEmitterRef<MouseEvent> = output<MouseEvent>()
+
+    handleClick(event: MouseEvent): void {
+        event.preventDefault();
+        event.stopPropagation();
+        this.click.emit(event);
+    }
 }
