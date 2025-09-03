805e8b5185bff85551a79d4b653f52b89386261e
Pavlo Satyrenko <zevsp09@gmail.com>
2025-09-03 11:38:28 +0300

    feat(select): implement select component with styling and change handling

---
+++ src/app/shared/components/select/select.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from "@angular/common";
+import { Component, input, InputSignal, model, ModelSignal } from "@angular/core";
+
+@Component({
+    selector: "ui-select",
+    imports: [CommonModule],
+    templateUrl: "./select.html",
+    styleUrl: "./select.css"
+})
+export class Select {
+    public readonly label: InputSignal<string> = input<string>("");
+    public readonly size: InputSignal<"small" | "large"> = input<"small" | "large">("small");
+    public readonly theme: InputSignal<"primary"> = input<"primary">("primary");
+    public readonly isDisabled: InputSignal<boolean> = input<boolean>(false);
+    public readonly isFullWidth: InputSignal<boolean> = input<boolean>(false);
+    public readonly isInline: InputSignal<boolean> = input<boolean>(false);
+
+    public value: ModelSignal<string> = model<string>("");
+
+    handleChange(event: Event): void {
+        const newValue: string = (event.target as HTMLSelectElement).value;
+        this.value.set(newValue);
+    }
+}
