d36e6952d1e1c9569336a0ddf5cd05733761350c
PavloSatyrenko <zevsp09@gmail.com>
2025-10-19 23:17:28 +0300

    feat: integrate ConferenceWebsocket for dynamic participant updates and local stream management

--- src/app/components/conference/room/room.ts
+++ src/app/components/conference/room/room.ts
@@ -1,8 +1,10 @@
-import { Component, effect, ElementRef, Signal, signal, viewChild, WritableSignal } from "@angular/core";
+import { Component, effect, ElementRef, inject, Signal, signal, untracked, viewChild, WritableSignal } from "@angular/core";
 import { Participant } from "@components/conference/participant/participant";
 import { ControlsItem } from "@components/conference/controls-item/controls-item";
 import { ParticipantType } from "@shared/types/ParticipantType";
 import { ConferenceControlsItemType } from "@shared/types/ConferenceControlsItemType";
+import { ConferenceWebsocket } from "@shared/services/conference-websocket";
+import { StreamsType } from "@shared/types/StreamsType";
 
 @Component({
     selector: "app-conference-room",
@@ -19,6 +21,8 @@ export class Room {
 
     protected controlsItems: WritableSignal<ConferenceControlsItemType[]> = signal<ConferenceControlsItemType[]>(["audio", "video", "screen", "participants", "chat", "hand", "other", "leave"]);
 
+    private conferenceWebSocket: ConferenceWebsocket = inject(ConferenceWebsocket);
+
     constructor() {
         effect(() => {
             if (this.participantsWrapper()) {
@@ -31,68 +35,41 @@ export class Room {
                 this.participantsWrapper()!.nativeElement.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
                 this.participantsWrapper()!.nativeElement.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
 
-                if (participantsAmount % 2) {
+                if (participantsAmount % 2 && rows > 1) {
                     for (let i = 0; i < participantsAmount - (rows - 1) * columns; i++) {
                         this.participants()[(rows - 1) * columns + i].isMoved = true;
                     }
                 }
             }
         });
-    }
 
-    ngOnInit(): void {
-        this.participants.set([
-            {
-                id: "1",
+        effect(() => {
+            const remoteStreams: Record<string, StreamsType> = this.conferenceWebSocket.remoteStreams();
+
+            const updatedParticipants: ParticipantType[] = Object.entries(remoteStreams).map(([socketId, streams]: [string, StreamsType]) => ({
+                id: socketId,
+                name: `User ${socketId.substring(0, 5)}`,
+                isAudioEnabled: true,
+                isVideoEnabled: true,
+                audioStream: streams.audioStream,
+                videoStream: streams.videoStream,
+                isMoved: false,
+                isLocal: false,
+            }));
+
+            updatedParticipants.push({
+                id: "local",
                 name: "You",
-                isAudioEnabled: false,
-                isVideoEnabled: false,
-                audioStream: new MediaStream(),
-                videoStream: new MediaStream(),
+                isAudioEnabled: true,
+                isVideoEnabled: true,
+                audioStream: this.conferenceWebSocket.localAudioStream(),
+                videoStream: this.conferenceWebSocket.localVideoStream(),
                 isMoved: false,
                 isLocal: true
-            },
-            {
-                id: "2",
-                name: "Participant 2",
-                isAudioEnabled: false,
-                isVideoEnabled: false,
-                audioStream: new MediaStream(),
-                videoStream: new MediaStream(),
-                isMoved: false,
-                isLocal: false
-            },
-            {
-                id: "3",
-                name: "Participant 3",
-                isAudioEnabled: false,
-                isVideoEnabled: false,
-                audioStream: new MediaStream(),
-                videoStream: new MediaStream(),
-                isMoved: false,
-                isLocal: false
-            },
-            {
-                id: "4",
-                name: "Participant 4",
-                isAudioEnabled: false,
-                isVideoEnabled: false,
-                audioStream: new MediaStream(),
-                videoStream: new MediaStream(),
-                isMoved: false,
-                isLocal: false
-            },
-            {
-                id: "5",
-                name: "Participant 5",
-                isAudioEnabled: false,
-                isVideoEnabled: false,
-                audioStream: new MediaStream(),
-                videoStream: new MediaStream(),
-                isMoved: false,
-                isLocal: false
-            }
-        ]);
+            });
+
+            this.participants.set(updatedParticipants);
+        });
     }
 
     getOptimalParticipantsLayout(wrapperElement: HTMLElement, participantsAmount: number): { rows: number, columns: number } {
