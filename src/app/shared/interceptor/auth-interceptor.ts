da0ff846a39ada56026360ca28c1b0ef8a6f28c1
PavelSatyrenko <40754638+PavloSatyrenko@users.noreply.github.com>
2025-11-12 18:09:25 +0200

    feat: implement authentication service with login, signup, and token refresh functionality; add auth interceptor

---
+++ src/app/shared/interceptor/auth-interceptor.ts
@@ -0,0 +1,35 @@
+import { HttpErrorResponse, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from "@angular/common/http";
+import { inject } from "@angular/core/primitives/di";
+import { environment } from "@shared/environments/environment";
+import { AuthService } from "@shared/services/auth.service";
+import { throwError } from "rxjs/internal/observable/throwError";
+import { catchError } from "rxjs/internal/operators/catchError";
+import { switchMap } from "rxjs/internal/operators/switchMap";
+
+export const authInterceptor: HttpInterceptorFn = (request: HttpRequest<any>, next: HttpHandlerFn) => {
+    const authService: AuthService = inject(AuthService);
+
+    let newRequest: HttpRequest<any> = request;
+
+    if (request.url.startsWith(environment.serverURL)) {
+        newRequest = request.clone({
+            withCredentials: true,
+        });
+    }
+
+    return next(newRequest).pipe(
+        catchError((error: HttpErrorResponse) => {
+            if (error.status === 401) {
+                return authService.refreshToken().pipe(
+                    switchMap(() => next(newRequest)),
+                    catchError((refreshError: any) => {
+                        authService.user.set(null);
+                        return throwError(() => refreshError);
+                    })
+                )
+            }
+
+            return throwError(() => error);
+        })
+    );
+};
---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,64 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable, signal, WritableSignal } from "@angular/core";
+import { environment } from "@shared/environments/environment";
+import { UserType } from "@shared/types/UserType";
+import { firstValueFrom, Observable } from "rxjs";
+
+@Injectable({
+    providedIn: "root",
+})
+export class AuthService {
+    public user: WritableSignal<UserType | null> = signal<UserType | null>(null);
+
+    private httpClient: HttpClient = inject(HttpClient);
+
+    public async signUp(email: string, name: string, surname: string, password: string): Promise<UserType> {
+        return await firstValueFrom(this.httpClient.post<UserType>(environment.serverURL + "/auth/signup", {
+            email,
+            name,
+            surname,
+            password
+        })).then((user: UserType) => {
+            this.getAuthenticatedUser();
+
+            return user;
+        });
+    }
+
+    public async logIn(email: string, password: string): Promise<UserType> {
+        return await firstValueFrom(this.httpClient.post<UserType>(environment.serverURL + "/auth/login", {
+            email,
+            password
+        })).then((user: UserType) => {
+            this.getAuthenticatedUser();
+
+            return user;
+        });
+    }
+
+    public async getAuthenticatedUser(): Promise<UserType> {
+        return await firstValueFrom(this.httpClient.get<UserType>(environment.serverURL + "/auth/user"))
+            .then((user: UserType) => {
+                this.user.set(user);
+                return user;
+            });
+    }
+
+    public async logOut(): Promise<void> {
+        return await firstValueFrom(this.httpClient.post<void>(environment.serverURL + "/auth/logout", {}))
+            .then(() => {
+                this.user.set(null);
+            });
+    }
+
+    public refreshToken(): Observable<void> {
+        return this.httpClient.post<void>(environment.serverURL + "/auth/refresh", {});
+    }
+
+    public async init(): Promise<void> {
+        await this.getAuthenticatedUser()
+            .catch(() => {
+                this.user.set(null);
+            });
+    }
+}
