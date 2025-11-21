d0d00910aef4ed36082998a306ba9f72a15a6397
PavelSatyrenko <40754638+PavloSatyrenko@users.noreply.github.com>
2025-11-21 21:44:33 +0200

    feat: implement chat message retrieval and local storage in IndexedDB

--- src/app/shared/services/chat-websocket.ts
+++ src/app/shared/services/chat-websocket.ts
@@ -3,6 +3,15 @@ import { environment } from "@shared/environments/environment";
 import { io, Socket } from "socket.io-client";
 import { ChatsService } from "./chats.service";
 import { ChatType } from "@shared/types/ChatType";
+import { ChatMessageType } from "@shared/types/ChatMessageType";
+import { openDB, DBSchema, IDBPDatabase } from "idb";
+
+interface ChatDB extends DBSchema {
+    messages: {
+        key: string;
+        value: ChatMessageType[];
+    }
+}
 
 @Injectable({
     providedIn: "root",
@@ -10,8 +19,17 @@ import { ChatType } from "@shared/types/ChatType";
 export class ChatWebsocket {
     private socket!: Socket;
 
+    private indexedDB: Promise<IDBPDatabase<ChatDB>> = openDB<ChatDB>("chatDatabase", 1, {
+        upgrade(db) {
+            db.createObjectStore("messages");
+        }
+    });;
+
     private internalChats: WritableSignal<ChatType[]> = signal([]);
     public chats: Signal<ChatType[]> = computed(() => this.internalChats());
+
+    private internalChatMessages: WritableSignal<Record<string, ChatMessageType[]>> = signal({});
+    public chatMessages: Signal<Record<string, ChatMessageType[]>> = computed(() => this.internalChatMessages());
 
     private chatsService: ChatsService = inject(ChatsService);
 
@@ -21,6 +39,35 @@ export class ChatWebsocket {
         this.chatsService.getChats()
             .then((chats: ChatType[]) => {
                 this.internalChats.set(chats);
+            });
+    }
+
+    public async getMessagesForChat(chatUserId: string): Promise<void> {
+        const indexedDB: IDBPDatabase<ChatDB> = await this.indexedDB;
+        
+        const storedMessages: ChatMessageType[] = (await indexedDB.get("messages", chatUserId)) ?? [];
+
+        this.internalChatMessages.update((messages: Record<string, ChatMessageType[]>) => ({
+            ...messages,
+            [chatUserId]: storedMessages,
+        }));
+
+        const lastMessageId: string | null = storedMessages.length > 0 ? storedMessages[storedMessages.length - 1].id : null;
+
+        await this.chatsService.getMessages(chatUserId, lastMessageId)
+            .then(async (newMessages: ChatMessageType[]) => {
+                if (newMessages.length === 0) {
+                    return;
+                }
+
+                this.internalChatMessages.update((messages: Record<string, ChatMessageType[]>) => ({
+                    ...messages,
+                    [chatUserId]: [...(messages[chatUserId] || []), ...newMessages],
+                }));
+
+                const last20Messages: ChatMessageType[] = [...storedMessages, ...newMessages].slice(-20);
+
+                await indexedDB.put("messages", last20Messages, chatUserId);
             });
     }
 }
--- src/app/shared/services/chats.service.ts
+++ src/app/shared/services/chats.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { environment } from "@shared/environments/environment";
+import { ChatMessageType } from "@shared/types/ChatMessageType";
 import { ChatType } from "@shared/types/ChatType";
 import { firstValueFrom } from "rxjs";
 
@@ -12,6 +13,18 @@ export class ChatsService {
 
     public async getChats(): Promise<ChatType[]> {
         return await firstValueFrom(this.httpClient.get<ChatType[]>(environment.serverURL + "/chats"));
+    }
+
+    public async getMessages(chatUserId: string, lastMessageId: string | null): Promise<ChatMessageType[]> {
+        const params: Record<string, string> = {};
+
+        if (lastMessageId) {
+            params["after"] = lastMessageId;
+        }
+
+        return await firstValueFrom(this.httpClient.get<ChatMessageType[]>(environment.serverURL + "/chats/" + chatUserId, {
+            params: params,
+        }));
     }
 
     public async sendMessage(receiverId: string, content: string): Promise<void> {
