6c37cd884b4991b176855d37ba094f74d7689a22
PavloSatyrenko <zevsp09@gmail.com>
2025-11-23 22:43:32 +0200

    feat: implement main layout with user authentication and meeting joining functionality

--- src/app/layouts/main/main.ts
+++ src/app/layouts/main/main.ts
@@ -1,9 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, computed, inject, signal, Signal, WritableSignal } from "@angular/core";
+import { Router, RouterLinkWithHref } from "@angular/router";
+import { AuthService } from "@shared/services/auth.service";
+import { Button } from "@shared/components/button/button";
+import { Title } from "@shared/components/title/title";
+import { Input } from "@shared/components/input/input";
 
 @Component({
     selector: "app-layout-main",
-    imports: [],
+    imports: [Button, RouterLinkWithHref, Title, Input],
     templateUrl: "./main.html",
     styleUrl: "./main.css",
 })
-export class Main { }
+export class Main {
+    protected isUserAuthorized: Signal<boolean> = computed(() => !!this.authService.user());
+
+    protected meetingCodeValue: WritableSignal<string> = signal<string>("");
+
+    private authService: AuthService = inject(AuthService);
+    private router: Router = inject(Router);
+
+    protected joinConference(): void {
+        if (this.meetingCodeValue().trim().length === 0) {
+            return;
+        }
+
+        this.router.navigate(["/conference", this.meetingCodeValue()])
+    }
+}
