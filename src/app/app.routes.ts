630cfd814dc761dead8d746c5115fa94aa50e11d
PavloSatyrenko <zevsp09@gmail.com>
2025-11-24 14:52:16 +0200

    feat: update routing to redirect to 'redirect' and change signup link to 'workspace'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -35,7 +35,7 @@ export const routes: Routes = [
             },
             {
                 path: "**",
-                redirectTo: ""
+                redirectTo: "redirect",
             }
         ]
     }
