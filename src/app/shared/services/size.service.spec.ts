7b7a4954ef0a91521579d0780264212d92c2eda7
PavloSatyrenko <zevsp09@gmail.com>
2025-12-07 21:41:18 +0200

    test: enhance SizeService tests with HttpTestingController and appConfig providers

--- src/app/shared/services/size.service.spec.ts
+++ src/app/shared/services/size.service.spec.ts
@@ -1,16 +1,27 @@
 import { TestBed } from "@angular/core/testing";
 
 import { SizeService } from "./size.service";
+import { HttpTestingController, provideHttpClientTesting } from "@angular/common/http/testing";
+import { appConfig } from "app.config";
 
 describe("SizeService", () => {
     let service: SizeService;
+    let httpMock: HttpTestingController;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            providers: [...appConfig.providers, provideHttpClientTesting()],
+            teardown: { destroyAfterEach: false }
+        });
         service = TestBed.inject(SizeService);
+        httpMock = TestBed.inject(HttpTestingController);
     });
 
     it("should be created", () => {
         expect(service).toBeTruthy();
+    });
+
+    afterEach(() => {
+        httpMock.verify();
     });
 });
